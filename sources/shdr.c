e1c70702762f40934cf4d9de6a65df74603c1e9b
aweaver <aweaver@student.42.fr>
2024-09-20 17:29:53 +0200

    ADD SHDR CHECKS:
    Adding shdr checks.
    
    Need to work on checking alignment.
    Need to work on checking symtable.
    Need to work on checking symbol string table.

--- sources/shdr.c
+++ sources/shdr.c
@@ -1,3 +1,9 @@
+#include <elf.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "ft_nm.h"
+
+
 	//ElfN_Off	e_shoff; 		-> triggers bfd errors
 		//uint32_t	e_flags; 	-> not checked by nm
 		//uint16_t	e_ehsize; 	-> not checked by nm
@@ -20,8 +26,9 @@
  * @param file t_mem storing mapped file and infos
  * @return non-zero in case of error
 */
-int	check_shdr(t_mem *file)
+int	check_shdr(const t_mem *file)
 {
+	printf("DEBUG: check shdr\n");
 	if (file->class == ELFCLASS64 && file->endianness == ELFDATA2LSB)
 	{
 		if (check_shdr_64lsb(file) != ERROR)
@@ -29,21 +36,87 @@ int	check_shdr(t_mem *file)
 	}
 	if (file->class == ELFCLASS32 && file->endianness == ELFDATA2LSB)
 	{
-		if (check_shdr_32lsb(file, (Elf32_Phdr *)file->raw) != ERROR)
+		if (check_shdr_32lsb(file) != ERROR)
 			return (0);
 	}
 	if (file->class == ELFCLASS64 && file->endianness == ELFDATA2MSB)
 	{
-		if (check_shdr_64msb(file, (Elf64_Phdr *)file->raw) != ERROR)
+		if (check_shdr_64msb(file) != ERROR)
 			return (0);
 	}
 	if (file->class == ELFCLASS32 && file->endianness == ELFDATA2MSB)
 	{
-		if (check_shdr_32msb(file, (Elf32_Phdr *)file->raw) != ERROR)
+		if (check_shdr_32msb(file) != ERROR)
 			return (0);
 	}
 	return (ERROR);
 }
 
+/*
+ *	@brief
+ *
+ *	@param
+ *	@return
+*/
+int	check_shdr_64lsb(const t_mem *file)
+{
+	bool				section_too_long = false;
+	const Elf64_Ehdr	*ehdr = (Elf64_Ehdr *)file->raw;
+	const Elf64_Shdr	*shdr_table = (Elf64_Shdr *)&(file->raw[ehdr->e_shoff]);
 
+	printf("called shdr 64 lsb\n");
+	for (int i = 0; i < ehdr->e_shnum; i++)
+	{
+		const Elf64_Shdr shdr = shdr_table[i];
+		if (shdr.sh_type == SHT_NULL)
+			continue;
+		//check size of sections
+		if (shdr.sh_offset + shdr.sh_size > file->size)
+			section_too_long = true;
+		//linked section is within section list
+		if (shdr.sh_link > ehdr->e_shnum)
+			return (fprintf(stderr, "nm: %s: file format not recognized\n", file->name), ERROR);
+		//check that size of section and elements is not corrupt
+		if (shdr.sh_entsize && (shdr.sh_size % shdr.sh_entsize) != 0)
+			return (fprintf(stderr, "nm: %s: file format not recognized\n", file->name), ERROR);
+	}
+	if (section_too_long == true)
+		fprintf(stderr, "nm: warning: %s: has a section extending past end of file\n", file->name);
+	return (0);
+}
 
+/*
+ *	@brief
+ *
+ *	@param
+ *	@return
+*/
+int	check_shdr_32lsb(const t_mem *file)
+{
+	(void)file;
+	return (0);
+}
+
+/*
+ *	@brief
+ *
+ *	@param
+ *	@return
+*/
+int	check_shdr_64msb(const t_mem *file)
+{
+	(void)file;
+	return (0);
+}
+
+/*
+ *	@brief
+ *
+ *	@param
+ *	@return
+*/
+int	check_shdr_32msb(const t_mem *file)
+{
+	(void)file;
+	return (0);
+}
