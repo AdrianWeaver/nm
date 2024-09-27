c5f33ed16f77cd8ff37f2e3e8a092397c2f8b1ce
aweaver <aweaver@student.42.fr>
2024-09-27 11:43:57 +0200

    FIX TYPE FILTER:
    Fixing type filter, working on removing duplicates with @GLIB
    working on cleaning the tree too.

--- sources/types.c
+++ sources/types.c
@@ -5,7 +5,7 @@
 char get_symbol_type_64lsb(t_mem *file, Elf64_Sym *symbol, t_symbol *sym)
 {
 	static int i;
-	char *sh_type[] = { "SHT_NULL", "SHT_PROGBITS", "SHT_SYMTAB", "SHT_STRTAB", "SHT_RELA", "SHT_HASH", "SHT_DYNAMIC", "SHT_NOTE", "SHT_NOBITS", "SHT_REL", "SHT_SHLIB", "SH_DYNSYM" };
+	char *sh_type[] = { "SHT_NULL", "SHT_PROGBITS", "SHT_SYMTAB", "SHT_STRTAB", "SHT_RELA", "SHT_HASH", "SHT_DYNAMIC", "SHT_NOTE", "SHT_NOBITS", "SHT_REL", "SHT_SHLIB", "SHT_DYNSYM" , "12", "13", "SHT_INIT_ARRAY", "SHT_FINI_ARRAY", "SHT_PREINIT_ARRAY", "SHT_GROUP", "SHT_SYMTAB_SHNDX", "SHT_NUM" };
 	char *st_type[] = { "STT_NOTYPE", "STT_OBJECT", "STT_FUNC", "STT_SECTION", "STT_FILE", "STT_COMMON", "STT_TLS", "STT_NUM" };
 	char *st_bind[] = { "STB_LOCAL", "STB_GLOBAL", "STB_WEAK", "STB_NUM" };
 
@@ -21,7 +21,7 @@ char get_symbol_type_64lsb(t_mem *file, Elf64_Sym *symbol, t_symbol *sym)
 	uint64_t		mergeable_section = shdr->sh_flags & SHF_MERGE;
 	(void)sh_type;(void)st_bind;(void)st_type;
 
-	printf("%3d: %39s section: %3u type: %16u %13s %5s %8s %9s %9s symbol type: %13s bind: %s\n", i, sym->name, symbol->st_shndx, shdr->sh_type, (shdr->sh_type < 11) ? sh_type[shdr->sh_type]: "type offlimit", (executable_section) ? "exec" : "", (writable_section)? "writable" : "", (allocated_section) ? "allocated" : "", (mergeable_section) ? "mergeable" : "", (symbol_type < 8) ? st_type[symbol_type]: "st type offlimit", st_bind[symbol_bind]);
+	printf("%3d: %39s section: %3u type: %16u %14s %5s %8s %9s %9s symbol type: %13s bind: %s\n", i, sym->name, symbol->st_shndx, shdr->sh_type, (shdr->sh_type < 20) ? sh_type[shdr->sh_type]: "type offlimit", (executable_section) ? "exec" : "", (writable_section)? "writable" : "", (allocated_section) ? "allocated" : "", (mergeable_section) ? "mergeable" : "", (symbol_type < 8) ? st_type[symbol_type]: "st type offlimit", st_bind[symbol_bind]);
 	i++;
 	if (symbol->st_shndx == SHN_UNDEF)
 	{
@@ -41,37 +41,36 @@ char get_symbol_type_64lsb(t_mem *file, Elf64_Sym *symbol, t_symbol *sym)
 			return ('c');
 		return ('C');
 	}
-	//below is broken
-	if (allocated_section && writable_section)
+	if (symbol_bind == STB_WEAK)
 	{
-			if (symbol_bind == STB_LOCAL)
-				return ('d');
-			return ('D');
+		if (executable_section)
+			return ('W');
+		return ('V');
 	}
-	if (allocated_section && !writable_section)
-	{
-		if (symbol_bind == STB_LOCAL)
-			return ('r');
-		return ('R');
-	}
-	if (executable_section)
+	//.bss
+	if (shdr->sh_type == SHT_PROGBITS && symbol_type == STT_FUNC)
 	{
 		if (symbol_bind == STB_LOCAL)
 			return ('t');
 		return ('T');
+	}	if (writable_section && allocated_section && !executable_section && !mergeable_section)
+	{
+		if (shdr->sh_type == SHT_NOBITS)
+		{
+			if (symbol_bind == STB_LOCAL)
+				return ('b');
+			return ('B');
+		}
+
+		if (symbol_bind == STB_LOCAL)
+			return ('d');
+		return ('D');
 	}
-	//.bss
-	if (shdr->sh_type == SHT_NOBITS && allocated_section)
+	if (allocated_section && !writable_section && !mergeable_section && !executable_section)
 	{
 		if (symbol_bind == STB_LOCAL)
-			return ('b');
-		return ('B');
-	}
-	if (shdr->sh_type == SHT_PROGBITS && allocated_section && symbol_type == STT_FUNC)
-	{
-		if (symbol_bind == STB_LOCAL)
-			return ('i');
-		return ('I');
+			return ('r');
+		return ('R');
 	}
 	if (mergeable_section && allocated_section)
 	{
@@ -79,24 +78,26 @@ char get_symbol_type_64lsb(t_mem *file, Elf64_Sym *symbol, t_symbol *sym)
 			return ('g');
 		return ('G');
 	}
-
 	if (shdr->sh_type == SHT_NOBITS && allocated_section)
 	{
 		if (symbol_bind == STB_LOCAL)
 			return ('s');
 		return ('S');
 	}
-	if (shdr->sh_type == SHT_NOBITS && allocated_section)
+	if (!writable_section && !executable_section && !mergeable_section)
 	{
 		if (symbol_bind == STB_LOCAL)
 			return ('n');
 		return ('N');
 	}
-	if (symbol_bind == STB_WEAK)
+
+	//GNU SPECIFIC UNTESTED
+	if ((shdr->sh_type == SHT_REL || shdr->sh_type == SHT_RELA)
+			&& allocated_section && symbol_type == STT_FUNC)
 	{
-		if (executable_section)
-			return ('W');
-		return ('V');
+		if (symbol_bind == STB_LOCAL)
+			return ('i');
+		return ('I');
 	}
 	return ('?');
 }
